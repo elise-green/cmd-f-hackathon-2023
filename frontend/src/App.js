7735417115c26a2c98e8ae06863564200fc7a190
elise-green <greeneliseb@gmail.com>
2023-03-11 17:17:09 -0800

    working on the second panel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
+import  Second from "./Components/second-panel";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-         
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
