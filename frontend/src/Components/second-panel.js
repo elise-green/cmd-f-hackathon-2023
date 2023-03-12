61422bff35e74dd4358cb2a923e4153a49178b1c
elise-green <greeneliseb@gmail.com>
2023-03-11 18:09:49 -0800

    want to pull no real change

--- frontend/src/Components/second-panel.js
+++ frontend/src/Components/second-panel.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import "./second-panel.css"
 function Second() {
+
     return( 
         <div className='.container'>
+            <button > Back</button>
             <container>
                 <h2>SPOTIFY</h2>
             </container>
-            <button> Regenerate </button>
-            <button> Back</button>
+            <br/>
+            <br/>
+            <br/>
+            <button color='#F9B4F6'> Regenerate </button>
+           
         </div>
 )
     ;
