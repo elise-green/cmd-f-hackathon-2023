ae0cb27528274bcfca4c7d12de408f8ceed268f8
janaayman <janayman@student.ubc.ca>
2023-03-11 16:26:00 -0800

    Added first intro page for the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Intro from "./Components/first-panel"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-         
-        >
-          Learn React
-        </a>
-      </header>
+      <Intro></Intro>
     </div>
   );
 }
---
+++ frontend/src/Components/first-panel.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./first-panel.css"
+import { useState, useEffect } from "react"
+
+function Intro() {
+    return (
+      <div className="container">
+        <br />  <br /> <br /> <br /> <br /> <br /> <br /> <br />
+        <h1 style={{ color: '#A3FEB4', fontFamily: 'Kaisei Tokumin'}}>
+            How was your day today?</h1>
+            <form>
+                <label style={{color:'white', fontFamily: 'Arial', fontWeight: 'bold'}}>Write 2-3 sentences about your day so that 
+                    we can generate a personalized spotify playlist just for you!</label>
+                <br />
+                <br />
+                <input
+        type="text"
+        name="user-response"
+        style={{ width: '500px', height: '200px' }}
+      />
+            </form>
+            <button></button>
+      </div>
+    )
+  }
+
+export default Intro;
