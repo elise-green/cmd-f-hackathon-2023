acab1ff340a9a5e40f2ceef3cb3dfd99cc79c0bc
elise-green <greeneliseb@gmail.com>
2023-03-11 19:48:51 -0800

    buttons git push

--- frontend/src/Components/second-panel.js
+++ frontend/src/Components/second-panel.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import "./second-panel.css"
-function Second() {
+function Second(props) {
 
+    function submitHandler() {
+        props.setPage("firstPage")
+    }
     return( 
-        <div className='.container'>
-            <button > Back</button>
+        <div className='container'>
+            <button type='submit' onClick={submitHandler}  style={{backgroundColor: "#FC8EAC", float: 'left', width: "60px", height:"30px", fontSize: "20px"}} > Back</button>
             <container>
-                <h2>SPOTIFY</h2>
+            
             </container>
-            <br/>
-            <br/>
-            <br/>
-            <button color='#F9B4F6'> Regenerate </button>
+            <br/> <br/><br/><br/><br/><br/><br/> <br/><br/> <br/> <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <button  style ={{backgroundColor: "#FC8EAC",  width: "120px", height:"30px", fontSize: "20px"}} > Regenerate </button>
            
         </div>
 )
