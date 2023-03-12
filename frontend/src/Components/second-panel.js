44573b076ce53ee25c66beb394ca8138e2d13b4a
janaayman <janayman@student.ubc.ca>
2023-03-11 19:54:19 -0800

    trying to pull latest changes

--- frontend/src/Components/second-panel.js
+++ frontend/src/Components/second-panel.js
@@ -5,6 +5,12 @@ function Second() {
         <div className='.container'>
             <container>
                 <h2>SPOTIFY</h2>
+                <iframe title="myFrame"
+                style={{borderRadius:'12px'}}
+                src="https://open.spotify.com/embed/playlist/7hzUdiqw27hdWI5IhkkoUB?utm_source=generator"
+                 width="80%" height="500"
+                 allow="autoplay; clipboard-write; encrypted-media; 
+                 fullscreen; picture-in-picture" loading="lazy"></iframe>
             </container>
             <button> Regenerate </button>
             <button> Back</button>
