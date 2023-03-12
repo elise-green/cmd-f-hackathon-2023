eb36803cc144c0a840d983049b7f6ce1f721f1fa
kendice <kendiceyeungnm@gmail.com>
2023-03-11 16:59:08 -0800

    modified first-panel

--- frontend/src/Components/first-panel.js
+++ frontend/src/Components/first-panel.js
@@ -6,20 +6,19 @@ function Intro() {
     return (
       <div className="container">
         <br />  <br /> <br /> <br /> <br /> <br /> <br /> <br />
-        <h1 style={{ color: '#A3FEB4', fontFamily: 'Kaisei Tokumin'}}>
-            How was your day today?</h1>
+        <h1 style={{ color: '#A3FEB4', fontFamily: 'Kaisei Tokumin'}}>How was your day today?</h1>
             <form>
                 <label style={{color:'white', fontFamily: 'Arial', fontWeight: 'bold'}}>Write 2-3 sentences about your day so that 
                     we can generate a personalized spotify playlist just for you!</label>
                 <br />
                 <br />
                 <input
-        type="text"
-        name="user-response"
-        style={{ width: '500px', height: '200px' }}
-      />
+                  type="text"
+                  name="user-response"
+                  style={{ width: '500px', height: '200px', opacity: 0.7, borderRadius: '15px' }}
+                /><br />
+                <button type='submit' style={{ borderRadius: '15px', marginTop: '20px', backgroundColor: '#A3FEB4', height: '30px', fontFamily: 'Arial', fontWeight: 'bold' }}>Generate</button>
             </form>
-            <button></button>
       </div>
     )
   }
