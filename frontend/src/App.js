a6c3a6ff41cb93a2de926493e08f4aa4568c3e06
janaayman <janayman@student.ubc.ca>
2023-03-11 18:01:44 -0800

    Added functionality to the button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,18 @@
 import './App.css';
 import  Second from "./Components/second-panel";
 import Intro from "./Components/first-panel"
+import { useState } from 'react';
 
 function App() {
+  const [page, setPage] = useState("firstPage")
+  const pageList = {
+    firstPage: <Intro setPage={setPage}/>,
+    secondPage: <Second/>
+  }
+
   return (
     <div className="App">
-      <Intro></Intro>
+      {pageList[page]}
     </div>
   );
 }
--- frontend/src/Components/first-panel.js
+++ frontend/src/Components/first-panel.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./first-panel.css"
-import { useState, useEffect } from "react"
-import { useNavigate } from "react"
 
-function Intro() {
+function Intro(props) {
+    function submitHandler() {
+        props.setPage("secondPage")
+    }
+
     return (
       <div className="container">
         <br />  <br /> <br /> <br /> <br /> <br /> <br /> <br />
@@ -16,14 +18,13 @@ function Intro() {
                 <input
                   type="text"
                   name="user-response"
-                  style={{ width: '500px', height: '200px', opacity: 0.7, borderRadius: '15px' }}
+                  style={{ width: '500px', height: '200px', opacity: 0.7, borderRadius: '15px', fontWeight: 'bold'}}
                 /><br />
-                <button type='submit' style={{ borderRadius: '15px', marginTop: '20px', backgroundColor: '#A3FEB4', height: '30px', fontFamily: 'Arial', fontWeight: 'bold' }}>Generate</button>
+                <button type='submit' onClick={submitHandler}
+                style={{ borderRadius: '15px', marginTop: '20px', backgroundColor: '#A3FEB4', height: '30px', fontFamily: 'Arial', fontWeight: 'bold' }}>
+                    Generate</button>
             </form>
             <br />
-            <button
-                onClick={() =>  window.location.href='/second-panel'}>
-                Generate</button>
       </div>
     )
   }
